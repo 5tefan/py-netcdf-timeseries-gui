237838808434a592112327aa44302fcdaed23f2c
Stefan Codrescu <ssmmcc1@gmail.com>
2018-06-18 20:37:51 +0000

    dataset_container: add documentation

--- pyntpg/datasets_container.py
+++ pyntpg/datasets_container.py
@@ -14,6 +14,15 @@ class DatasetsContainer(QObject):
 
     @pyqtSlot(str, str)
     def open(self, name, path):
+        """
+        Open a new dataset with name and path to file. Closes a dataset if path is empty.
+
+        Note: this could be an existing dataset with a new file, or a never before seen dataset.
+
+        :param name: string name of dataset
+        :param path: Path to dataset file
+        :return: None
+        """
         if path == "":
             self.close(name)
         else:
@@ -25,6 +34,13 @@ class DatasetsContainer(QObject):
 
     @pyqtSlot(str, str)
     def rename(self, before, after):
+        """
+        Rename a dataset.
+
+        :param before: string name of dataset to rename
+        :param after: string name of dataset after rename
+        :return: None
+        """
         # check that before is in keys, in case of, eg dataset rename
         # before any files have been opened...
         if before in self.datasets.keys():
@@ -33,16 +49,36 @@ class DatasetsContainer(QObject):
 
     @pyqtSlot(str, str)
     def close(self, name):
+        """
+        Receive indication that a dataset has closed or is no longer available.
+
+        :param name: string name of dataset to close
+        :return: None
+        """
         # here too, tab can be closed before any data was ever opened in it.
         if name in self.datasets.keys():
             self.datasets.pop(name)
             self.sig_closed.emit(name)
 
     def list_datasets(self):
-        """ Get a list of the datasets available. """
+        # type: () -> list[str]
+        """
+        :return: list of datasets available.
+        """
         return list(self.datasets.keys())
 
     def list_variables(self, dataset):
+        # type: (str) -> list[str]
+        """
+        Convenience function to get a list of variables avaialble in some dataset.
+
+        Motivation: currently, this app only supports Netcdf datasets, but if it were
+        to be extended to support other storage formats in the future, it would be nice
+        if functionality to get a list of variables from a dataset was implemented in one place.
+
+        :param dataset: name of dataset to get available variables from.
+        :return: list of variables available in a dataset
+        """
         if dataset in self.datasets.keys():
             # otherwise, it's a netcdf dataset, open and list the keys
             return self.datasets[dataset].variables.keys()
